11ac6ed7e20e14077e9710dae93b6de824a23cce
EDR3X <dhunganaannanta@gmail.com>
2022-08-03 14:53:18 +0545

    oopsi

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=20);
 
     loop {
-        println!("Please input your guess:");
+        println!("Please enter your guess:");
 
         let mut guess: String = String::new();
 
